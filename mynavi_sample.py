587923e7af528c52a31ebc92c8701c5db4aa46bc
1234kyota <tbhthltc1985@gmail.com>
2021-04-12 14:29:41 +0900

    kadai3

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -53,56 +53,74 @@ def main():
         "topSearch__text").send_keys(search_keyword)
     # 検索ボタンクリック
     driver.find_element_by_class_name("topSearch__button").click()
+   # 検索結果の結果の各コンテンツを取得
 
-        # 検索結果の結果の各コンテンツを取得
-
-    contents = driver.find_elements_by_class_name("cassetteRecruit__content")
     company_list = []
     
-    # contents_count = len(contents)
-    # 3つの要素を取得しリスト化
-    for content in contents[:10]:
+    i = 0
 
-    #  各会社の情報のリスト作成
-        company = []
-    #  各会社の名前をリストに追加
-        name_text = content.find_element_by_class_name("cassetteRecruit__name").text
-        company.append(name_text)
+    while True:
+        i += 1
+        # contents_count = len(contents)
+        contents = driver.find_elements_by_class_name("cassetteRecruit__content")
+
+        time.sleep(3)
+
+        #   3つの要素を取得しリスト化
+
+
+        #   コンテンツ取得
+        for content in contents[:3]:
+
+        #  各会社の情報のリスト作成
+            company = []
+        #  各会社の名前をリストに追加
+            name_text = content.find_element_by_class_name("cassetteRecruit__name").text
+            company.append(name_text)
+
+        
+        #  各会社の仕事内容、初任給をリストか
+
+        #  会社情報を取得
+            explanation_list = content.find_elements_by_class_name("tableCondition__head")
+
+        #  会社情報をテキスト化してリスト化
+            explanation_text_list = []
+
+            for explanation in explanation_list:
+                explanation_text_list.append(explanation.text)
+
+        #  テキスト化された会社情報をcompanyリストへ追加
+            if "仕事内容" in explanation_text_list:
+                work = content.find_elements_by_class_name("tableCondition__body")[0].text
+                company.append(work)
+            else:
+                company.append('未記載')
+
+            if "初年度年収" in explanation_text_list:
+                salary = content.find_elements_by_class_name("tableCondition__body")[4].text
+                company.append(salary)
+            else:
+                company.append('未記載')
+            
+            
+            company_list.append(company)
+
+
+            
+        if i > 2:
+            break
+        
+
+            # 次へボタンクリック
+        driver.find_element_by_class_name("iconFont--arrowLeft").click()
+       
+
 
     
-    #  各会社の仕事内容、初任給をリストか
-
-    #  会社情報を取得
-        explanation_list = content.find_elements_by_class_name("tableCondition__head")
-
-    #  会社情報をテキスト化してリスト化
-        explanation_text_list = []
-
-        for explanation in explanation_list:
-            explanation_text_list.append(explanation.text)
-
-    #  テキスト化された会社情報をcompanyリストへ追加
-        if "仕事内容" in explanation_text_list:
-            work = content.find_elements_by_class_name("tableCondition__body")[0].text
-            company.append(work)
-        else:
-            company.append('未記載')
-
-        if "初年度年収" in explanation_text_list:
-            salary = content.find_elements_by_class_name("tableCondition__body")[4].text
-            company.append(salary)
-        else:
-            company.append('未記載')
-        
-        company_list.append(company)
-
-        
-
-
     print(len(company_list))
-    print(company_list)
-  
-
+    for conmpany in company_list:
+        print(conmpany)
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
 if __name__ == "__main__":
